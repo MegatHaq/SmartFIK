7888375a3f3e17aee72bfd96851ead34300ac6e8
ellscott <dnmegatmehat@gmail.com>
2024-11-01 16:10:33 +0800

    Added charts

--- src/app/tutor/components/details.js
+++ src/app/tutor/components/details.js
@@ -1,7 +1,11 @@
+import { Component } from "./chart";
+
 export function Details() {
   return (
     <div className="flex gap-4">
-      <div className="bg-[red] w-3/6">This is where the pie chart is.</div>
+      <div className="w-3/6">
+        <Component />
+      </div>
       <div className="bg-[blue] w-full">This where the table will be.</div>
     </div>
   );
